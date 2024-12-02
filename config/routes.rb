6445619516b319acb7339c63bff3f1c8866a6f5c
ChoiceG <efechoice@gmail.com>
2024-12-02 16:47:04 +0100

    Added custom contact us page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get "about", to: "pages#about"
 
   resources :contacts
+
+  get "contact-us", to: "contacts#new"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
